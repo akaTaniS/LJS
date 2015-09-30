f46c0a330b1124ecb1752507ddfd6ec409f8b5c7
akaTaniS <akatanisis@gmail.com>
2015-09-30 20:02:48 +0300

    добавлены файлы

---
+++ 1.js
@@ -0,0 +1,12 @@
+<script>
+ var brouser = prompt('Введите ваш браузер')
+if (brouser == 'EI') {
+  alert('О, да у вас IE!')
+};
+else if (brouser == 'Chrome' || 'Firefox' || 'Safari' || 'Opera') {
+  alert('Да, и эти браузеры мы поддерживаем')
+}
+else {
+  alert('Мы надеемся, что и в вашем браузере все ок!')
+}
+</script>
